2654da3e8ccd2fd8d15ded18b818af39d6cb16a7
Sam Wade Keamy <swk57@cornell.edu>
2026-04-09 13:31:53 -0400

    forgot

---
+++ verilog/modules/row_writer.sv
@@ -0,0 +1,59 @@
+`ifndef READER_VH
+/* verilog_format: off */
+`define READER_VH
+/* verilog_format: on */
+
+// talk to qsys attached memory --> put in array
+module row_writer  #(parameter int N = 32)
+                    (
+                        output          [9:0]   mem_write_addr,
+                        output  signed  [15:0]  mem_write_data,
+                        output                  write_done,
+                        output                  we,
+                        input   signed  [15:0]  row [N],
+                        input           [9:0]   start_addr,
+                        input                   start_write,
+                        input                   is_column
+                    );
+    // row array is the actual registers holding row
+    logic signed [15:0] row_arr [N];
+    assign row = row_arr;
+    // 1-hot select logic for loading
+    logic [N-1:0] row_reg_sel;
+
+    assign mem_write_data = row_arr[mem_write_addr];
+
+    // here's how this horrible logic works:
+    // state = rst, waiting to start a write
+    // state = 0...N-1 do the write i
+    // fsm to write memory
+    logic [N-1:0] state_mem_write;
+    // this is just a counter for write addr
+    logic [$clog2(N)-1:0] mem_write_addr_reg;
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            state_mem_write <= 0;
+            mem_write_addr_reg <= 0;
+        end
+        else begin
+            if (start_write) begin
+                row_arr             <= row;
+                state_mem_write     <= 1;
+                mem_write_addr_reg  <= start_addr;
+            end
+            else begin
+                state_mem_write     <= state_mem_write << 1;
+                mem_write_addr_reg  <= is_column ? mem_write_addr_reg + N : mem_write_addr_reg + 1;
+            end
+        end
+    end
+
+    // enable writes until state mem gets to all zeroes
+    assign we = state_mem_write != 0;
+
+    // mem addr maps 1:1
+    assign mem_write_addr = mem_write_addr_reg;
+
+endmodule
+
+`endif
